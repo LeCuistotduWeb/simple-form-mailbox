4ef7f28c1f6945dcdd357ee1a3f7a8fba4b69a8e
LeCuistotduWeb <gaetan.boyron@gmail.com>
2019-05-24 14:51:28 +0200

    feat: add error alert

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,13 @@
     <v-content>
       <v-container>
         <v-layout class="my-3" row justify-space-between>
+        <TheSnackbar v-if="error" :text="error" :color="'error'"/>
           <v-flex xs9 sm10 md8 lg8 offset-md1>
-            <v-btn @click="loadMails()" color="success" class="ml-0">Boite de reception</v-btn>
-            <v-btn @click="loadSpams()" color="error">Spam</v-btn>
+            <v-layout row wrap align-center>
+              <v-btn @click="loadMails()" color="success" class="ml-0">Boite de reception</v-btn>
+              <v-btn @click="loadSpams()" color="error">Spam</v-btn>
+              <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details box />
+            </v-layout>
           </v-flex>
         </v-layout>
         <v-layout align-center justify-center>
@@ -14,7 +18,9 @@
             <v-data-table
               :headers="headers"
               :items="mails"
+              :pagination.sync="pagination"
               :loading="loading"
+              :search="search"
               class="elevation-1"
             >
               <template v-slot:items="props">
@@ -24,6 +30,7 @@
                 <td>{{ props.item.data.contact.message }}</td>
               </template>
             </v-data-table>
+            <p>Dernière actualisation: {{ latestUpload }}</p>
             <v-switch v-model="dark" primary label="Dark" />
           </v-flex>
         </v-layout>
@@ -35,23 +42,29 @@
 <script>
 import axios from 'axios'
 import TheNavbar from './components/TheNavbar'
+import TheSnackbar from './components/TheSnackbar'
 
 export default {
   name: 'App',
   components: {
     TheNavbar,
+    TheSnackbar,
   },
   data () {
     return {
       mails: [],
-        headers: [
-          { text: 'Date réception', value: 'created_at' },
-          { text: 'name', align: 'left', value: 'data.contact.name', sortable: false},
-          { text: 'email', value: 'data.contact.email', sortable: false },
-          { text: 'message', value: 'data.contact.message', sortable: false },
-        ],
-        loading: false,
-        dark: false,
+      headers: [
+        { text: 'Date réception', value: 'created_at', sortable: false },
+        { text: 'name', align: 'left', value: 'data.contact.name', sortable: false},
+        { text: 'email', value: 'data.contact.email', sortable: false },
+        { text: 'message', value: 'data.contact.message', sortable: false },
+      ],
+      pagination: {'sortBy': 'create_at', 'descending': true},
+      loading: false,
+      dark: false,
+      search: '',
+      error: null,
+      latestUpload: '',
     }
   },
   created() {
@@ -63,7 +76,11 @@ export default {
       axios.get(SIMPLEFORM_URL)
       .then(response => {
         this.loading = false
+        this.latestUpload = new Date()
         return this.mails = response.data
+      }).catch(() => {
+        this.mails = []
+        this.error = 'service indisponible'
       })
     },
     loadSpams() {
@@ -71,7 +88,11 @@ export default {
       axios.get(SIMPLEFORM_URL + '&spam=true')
       .then(response => {
         this.loading = false
+        this.latestUpload = new Date()
         return this.mails = response.data
+      }).catch(() => {
+        this.mails = []
+        this.error = 'service indisponible'
       })
     }
   },
---
+++ src/components/TheSnackbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-snackbar
+    v-model="snackbar"
+    :timeout="timeout"
+    :color="color"
+    :left="left"
+    :right="right"
+    :top="top"
+    :bottom="bottom"
+  >
+    {{ text }}
+    <v-btn
+      flat
+      @click="snackbar = false"
+    >
+      Close
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+    props:{
+        text: {
+            type: Boolean,
+            default: () => ''
+        },
+        timeout: {
+          type: Number,
+          default: () => 3000
+        },
+        left: Boolean,
+        right: Boolean,
+        top: Boolean,
+        bottom: Boolean,
+        color: String,
+    },
+    data() {
+        return {
+            snackbar: true,
+        }
+    },
+}
+</script>
