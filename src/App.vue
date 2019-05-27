6dfa377777a3dd5bf39d42bf4f60a38d91c163b7
LeCuistotduWeb <gaetan.boyron@gmail.com>
2019-05-27 12:28:28 +0200

    refactor datatable

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,25 @@
   <v-app :dark="dark">
       <TheNavbar/>
     <v-content>
-      <v-container>
+      <v-container fluid>
         <v-layout class="my-3" row justify-space-between>
         <TheSnackbar v-if="error" :text="error" :color="'error'"/>
-          <v-flex xs9 sm10 md8 lg8 offset-md1>
+          <v-flex md12 lg10 offset-lg1>
             <v-layout row wrap align-center>
-              <v-btn @click="loadMails()" color="success" class="ml-0">Boite de reception</v-btn>
-              <v-btn @click="loadSpams()" color="error">Spam</v-btn>
-              <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details box />
+              <v-flex xs12 sm6 md6>
+                <v-btn @click="loadMails()" color="success" class="ml-0">
+                <v-icon>cached</v-icon>
+                Raffraichir
+              </v-btn>
+              </v-flex>
+              <v-flex xs12 sm6 md6>
+                <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details box />
+              </v-flex>
             </v-layout>
           </v-flex>
         </v-layout>
         <v-layout align-center justify-center>
-          <v-flex xs12 md10 center>
+          <v-flex md12 lg10 center>
             <v-data-table
               :headers="headers"
               :items="mails"
@@ -24,10 +30,18 @@
               class="elevation-1"
             >
               <template v-slot:items="props">
-                <td>{{ props.item.created_at | formatDate }}</td>
-                <td>{{ props.item.data.contact.name }}</td>
-                <td>{{ props.item.data.contact.email }}</td>
-                <td>{{ props.item.data.contact.message }}</td>
+                <td>
+                  <div class="ma-3">
+                    <h3>{{ props.item.data.contact.name }}</h3>
+                    <p class="subheading">{{ props.item.data.contact.email }}</p>
+                    <p>Date de réception : {{ props.item.created_at | formatDate }}</p>
+                  </div>
+                </td>
+                <td>
+                  <div class="ma-3">
+                    {{ props.item.data.contact.message }}
+                  </div>
+                </td>
               </template>
             </v-data-table>
             <p>Dernière actualisation: {{ latestUpload }}</p>
@@ -54,10 +68,8 @@ export default {
     return {
       mails: [],
       headers: [
-        { text: 'Date réception', value: 'created_at', sortable: false },
-        { text: 'name', align: 'left', value: 'data.contact.name', sortable: false},
-        { text: 'email', value: 'data.contact.email', sortable: false },
-        { text: 'message', value: 'data.contact.message', sortable: false },
+        { text: 'Informations', sortable: false },
+        { text: 'Message', value: 'data.contact.message', sortable: false },
       ],
       pagination: {'sortBy': 'create_at', 'descending': true},
       loading: false,
@@ -83,18 +95,6 @@ export default {
         this.error = 'service indisponible'
       })
     },
-    loadSpams() {
-      this.loading = true
-      axios.get(SIMPLEFORM_URL + '&spam=true')
-      .then(response => {
-        this.loading = false
-        this.latestUpload = new Date()
-        return this.mails = response.data
-      }).catch(() => {
-        this.mails = []
-        this.error = 'service indisponible'
-      })
-    }
   },
   filters: {
     formatDate: function (value) {
@@ -108,3 +108,4 @@ export default {
 
 const SIMPLEFORM_URL = 'https://getsimpleform.com/messages.json?api_token=' + process.env.VUE_APP_SIMPLEFORM_TOKEN
 </script>
+
